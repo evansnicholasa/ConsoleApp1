4052e2c9b55617f4d9898d73158570acf9ebf878
Nicholas Evans <Nevans@nexient.com>
2019-12-13 16:33:05 -0500

    first push

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        /*  static void Main(string[] args)
+          {
+              Task<int> i = Task.Run(() =>
+             {
+
+             }).ContinueWith((Func<Task, int>)((i) =>
+                 {
+                     return (int)(i.Result * 2);
+                 }));
+          }
+  */
+
+
+
+        static void Main(string[] args)
+        {
+            string result = DownloadContent().Result;
+        }
+        public static async Task<string> DownloadContent()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string result = await client.GetStringAsync("http://www.microsoft.com");
+                return result;
+            }
+        }
+
+        // PLINQ, Concurrent COllections (Thread safe collections)
+
+    }
+}
